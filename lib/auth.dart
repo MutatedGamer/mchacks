b5ccf34529697ae79f8c0208cbefff4b91928bac
MutatedGamer <lukebordonaro@gmail.com>
2019-02-02 22:24:29 -0500

    added ability to add lessons

--- lib/auth.dart
+++ lib/auth.dart
@@ -5,15 +5,28 @@ part of './main.dart';
 final GoogleSignIn googleSignIn = GoogleSignIn();
 final FirebaseAuth auth = FirebaseAuth.instance;
 
-void _authenticateWithGoogle() async {
+Future<GoogleSignInAccount> getSignedInAccount(
+    GoogleSignIn googleSignIn) async {
+  // Is the user already signed in?
+  GoogleSignInAccount account = googleSignIn.currentUser;
+  // Try to sign in the previous user:
+  if (account == null) {
+    account = await googleSignIn.signInSilently();
+  }
+  return account;
+}
+
+Future<FirebaseUser> signIntoFirebase(
+    GoogleSignInAccount googleSignInAccount) async {
   final GoogleSignInAccount googleUser = await googleSignIn.signIn();
   final GoogleSignInAuthentication googleAuth =
   await googleUser.authentication;
   final AuthCredential creds = GoogleAuthProvider.getCredential(idToken: googleAuth.idToken,
       accessToken: googleAuth.accessToken);
   final FirebaseUser user = await
-  auth.signInWithCredential(creds
+   auth.signInWithCredential(creds
   );
+  return user;
   // do something with signed-in user
 }
 
---
+++ lib/firestore_api.dart
@@ -0,0 +1,21 @@
+part of './main.dart';
+
+void createLesson(String uid, Lesson lesson) {
+  CollectionReference lessons = Firestore.instance.collection('users')
+          .document(uid)
+          .collection('lessons');
+  Firestore.instance.runTransaction((Transaction tx) async {
+    var _result = await lessons.add(lesson.toJson());
+  });
+}
+
+void createBullet(String uid, String lesson_id, Bullet bullet ) {
+  CollectionReference lessons = Firestore.instance.collection('users')
+      .document(uid)
+      .collection('lessons')
+      .document(lesson_id)
+      .collection('regular_input');
+  Firestore.instance.runTransaction((Transaction tx) async {
+    var _result = await lessons.add(bullet.toJson());
+  });
+}
