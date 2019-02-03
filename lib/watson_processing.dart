9fb98b08d9b45b8cf1a0e6b761e78b6458f72775
Samantha Miller <m99@mit.edu>
2019-02-03 03:31:04 -0500

    working fill-in-the-blank generation

--- lib/watson_processing.dart
+++ lib/watson_processing.dart
@@ -1,52 +1,84 @@
 import 'package:http/http.dart' as http;
-import 'dart:io';
+import 'dart:async';
 import 'dart:convert';
 
-final String url = "https://gateway.watsonplatform.net/natural-language-understanding/api";
+final String baseUrl = "https://gateway.watsonplatform.net/natural-language-understanding/api";
 final String version = "/v1/analyze?version=2018-11-16";
 final String apiKey = "CrFaIlAl4J3tsFYqyUfKrvCxgg523kHgQBj_5ZZ6fgAw";
 
-void main() {
-  String features = "keywords";
-  String keywordsLimit = "keywords.limit=10";
-  String fullUrl = "$url$version&url=www.ibm.com&features=$features&$keywordsLimit";
-  Uri uri = Uri.parse(
-      "$url$version&url=www.ibm.com&features=$features&keywords.limit=10");
-  final auth = base64Encode(utf8.encode("apikey:$apiKey"));
+final auth = base64Encode(utf8.encode("apikey:$apiKey"));
 
-  Future future = http.get(fullUrl, headers: {"Authorization": "Basic $auth"})
-      .then((response) => print(response.body));
-//  http.get(fullUrl, headers: {"Authorization":"Basic $auth"});
-//  var client = new http.Client();
-//  var request = new http.Request("GET", uri);
-//  request.headers = {'authorization': "Basic $auth"};
-//  Future future = client.send(request).then((response)
-//  => response.stream.bytesToString().then((value)
-//  => print(value.toString()))).catchError((error) => print(error.toString()));
+class Watson {
 
+  /// Given a selection of the GET request parameters documented here:
+  /// https://console.bluemix.net/apidocs/natural-language-understanding#analyze-text-get
+  /// Returns the completed URL to be used for the GET request
+  static String buildURL(String text, List<String> featureList, Map<String, String> params) {
+    String encodedText = Uri.encodeQueryComponent(text);
+    String features = featureList.elementAt(0).toString();
+    for (var i = 1; i < featureList.length; i++) {
+      features += "," + featureList.elementAt(i).toString();
+    }
 
-//  var client = new http.Client();
-//  var request = new http.MultipartRequest('POST', uri);
-//  final auth = base64Encode(utf8.encode("apikey:$apiKey"));
-//  request.headers[HttpHeaders.contentTypeHeader] = 'application/json';
-//  request.headers['authorization'] = "Basic $auth";
-//
-//  request.fields['text'] = jsonEncode("IBM is an American multinational technology company headquartered in Armonk, New York, United States, with operations in over 170 countries.");
-//
-//  request.fields['features'] = jsonEncode({
-//    "entities": {
-//      "emotion": true,
-//      "sentiment": true,
-//      "limit": 2,
-//    },
-//    "keywords": {
-//      "emotion": false,
-//      "sentiment": false,
-//      "limit": 2,
-//    }
-//  });
-//
-//  Future future = client.send(request).then((response)
-//  => response.stream.bytesToString().then((value)
-//  => print(value.toString()))).catchError((error) => print(error.toString()));
+    String url = "$baseUrl$version&text=$encodedText&features=$features";
+    params.forEach((k,v) => url += "&$k=$v");
+
+    return url;
+  }
+
+  /// Makes a GET request, given a URL, and returns the response as a Future<Response>
+  static Future<http.Response> makeGETRequest(String url) async {
+    return http.get(Uri.parse(url), headers: {"Authorization":"Basic $auth"});
+  }
+
+  /// Returns the shortest keyword from a String as a Future<String>
+  static Future<String> getShortestKeyword(String text) async {
+    var response = await makeGETRequest(buildURL(text, ["keywords"], {"keywords.limit": "10"}));
+    var keywordList = json.decode(response.body)['keywords'];
+    List<String> keywords = new List();
+    for (Map<String, dynamic> keywordInfo in keywordList) {
+      keywords.add(keywordInfo['text']);
+    }
+    // find shortest keyword
+    String shortestKeyword = keywords[0];
+    for (String keyword in keywords) {
+      if (keyword.split(" ").length < shortestKeyword.split(" ").length) {
+        shortestKeyword = keyword;
+      }
+    }
+    return shortestKeyword;
+  }
+
+  /// Returns a fill-in-the-blank version of the given String where the provided
+  /// keyword is redacted.
+  static String createFillInTheBlankAsString(String text, String keyword) {
+    RegExp exp = new RegExp(r'\b'+"$keyword"+r'\b');
+    List<String> visibleWordsList = text.split(exp);
+    String result = visibleWordsList[0];
+    for (var i = 1; i < visibleWordsList.length; i++) {
+      result += "_____" + visibleWordsList[i];
+    }
+    return result;
+  }
+
+  static List<String> createFillInTheBlankAsList(String text, String keyword) {
+    RegExp exp = new RegExp(r'\b'+"$keyword"+r'\b');
+    List<String> visibleWordsList = text.split(exp);
+    return visibleWordsList;
+  }
+}
+
+void main() async {
+  String text1 = "George Washington was the first president of the USA.";
+  String keyword1 = await Watson.getShortestKeyword(text1);
+  print(Watson.createFillInTheBlankAsString(text1, keyword1));
+  print(Watson.createFillInTheBlankAsList(text1, keyword1));
+
+  String text2 = "The capital of Washington State is Olympia.";
+  String keyword2 = await Watson.getShortestKeyword(text2);
+  print(Watson.createFillInTheBlankAsString(text2, keyword2));
+
+  String text3 = "MIT is located in Cambridge, Massachusetts.";
+  String keyword3 = await Watson.getShortestKeyword(text3);
+  print(Watson.createFillInTheBlankAsString(text3, keyword3));
 }
